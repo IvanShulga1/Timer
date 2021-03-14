5b00022ba0cad2cdac2c5bfe79ef31d793f2b02d
Ivan Shulga <vanasulga878@gmail.com>
2021-03-14 22:28:06 +0300

    Добавьте файлы проекта.

---
+++ Models/TimerModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Timer.Models
+{
+    public class TimerModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+
+        private Guid _guidId;
+        public Guid GuidId { get => _guidId; set {_guidId = value; OnPropertyChanged(nameof(GuidId));} }
+
+        private bool _isPlayed;
+        public bool IsPlayed { get => _isPlayed; set { _isPlayed = value; OnPropertyChanged(nameof(IsPlayed)); } }
+    }
+}
---
+++ ViewModels/MainViewModel.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Timer.ViewModels
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+            }
+        }
+
+        Stopwatch _stopwatch;
+
+        private int? _hours;
+        public int? Hours
+        {
+            get => _hours;
+            set
+            {
+                OnPropertyChanged(nameof(Hours));
+                if (value > 0)
+                {
+                    _hours = value;
+                }
+            }
+        }
+
+        private int? _minutes;
+        public int? Minutes
+        {
+            get => _minutes;
+            set
+            {
+                if (value > 0)
+                {
+                    _minutes = value;
+                }
+                OnPropertyChanged(nameof(Minutes));
+            }
+        }
+
+        private int? _seconds;
+        public int? Seconds
+        {
+            get => _seconds;
+            set
+            {
+                if (value > 0)
+                {
+                    _seconds = value;
+                }
+                OnPropertyChanged(nameof(Seconds));
+            }
+        }
+    }
+}
